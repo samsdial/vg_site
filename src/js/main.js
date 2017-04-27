d06e9f68c30e66a7acb8f56a1de20140d86a9d4d
Sergio Martinez <dialstudios@gmail.com>
2017-04-27 12:42:42 -0500

    add menu home

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,75 @@
 /*jshint strict:true, browser:true, jquery:true */
-$(function() {
-});
+
+// #bucle while
+// Se vuelven a repertir hasta que sea falsa
+// <= menor o igual
+// >= mayor o igual
+//contador ++; o contador += 1; o contador = contador + 1;
+//while(mientras)(mientras que sea true)
+// var contador = 1;
+// while (contador <= 10){
+//     console.log(contador);
+//     contador ++;
+// }
+// #bucle for
+// for (var contador = 100; contador >= 1; contador--) {
+//     console.log(contador);
+// }
+// #bucle do while
+// do (naz) while (mientras)
+// var contador = 1;
+// do {
+//     console.log(contador);
+//     contador++;
+// }while(contador <=10);
+// ##Else
+// var edad = 14;
+
+// if ( edad == 14){
+//     alert('14 años.');
+// } else if (edad < 18){
+//     alert('Mayor de edad')
+// }
+// else if (edad > 65){
+//     alert('Supera edad');
+// }
+// else{
+//     alert('Mayor de edad');
+// }
+// alert
+// switch ( Cambiar ) !!importante siempre el break;
+// var premiado = false;
+// switch (premiado){
+//     case true:
+//         console.log('premiado');
+//         break;
+//     case false:{
+//         console.log('NO fue premiado');
+//         break;
+//     }
+// }
+var dia = 6;
+
+switch (dia) {
+    case 1:
+        console.log('Es lunes');
+        break;
+    case 2:
+        console.log('Es martes');
+        break;
+    case 3:
+        console.log('Es miercoles');
+        break;
+    case 4:
+        console.log('Es jueves');
+        break;
+    case 5:
+        console.log('Es viernes');
+        break;
+        default:
+            console.log('Sabado o domingo')
+}
+console.info('Resto del programa');
+
+// $(function() {
+// });
